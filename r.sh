b00c6aec00d71c0c9c82783f626f0e2067e118b7
metung <metung@spine.novalocal>
2024-01-21 22:16:50 +0800

    update readme & set question anchor

--- r.sh
+++ r.sh
@@ -3,5 +3,5 @@
 # $PWD only show current dir to execution
 scriptpath="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-docker run -it --rm -v /sys/fs/cgroup -v "$scriptpath"/nerdctl:/nerdctl --privileged --cap-add all nerdctl:test /scripts/s1_prepare.sh sh
-# docker run -it --rm -v "$scriptpath"/nerdctl:/nerdctl --privileged --cap-add all nerdctl:test /scripts/s1_prepare.sh sh
+# docker run -it --rm -v /sys/fs/cgroup -v "$scriptpath"/nerdctl:/nerdctl --privileged --cap-add all nerdctl:test /scripts/s1_prepare.sh sh
+docker run -it --rm -v "$scriptpath"/nerdctl:/nerdctl --privileged --cap-add all nerdctl:test /scripts/s1_prepare.sh sh
