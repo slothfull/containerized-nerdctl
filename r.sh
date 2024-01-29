62f2d8e25f8d938ee36af2aae0682034fcf21403
metung <metung@spine.novalocal>
2024-01-29 23:43:49 +0800

    update mod & readme & start script

--- r.sh
+++ r.sh
@@ -4,8 +4,7 @@
 spath="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 # docker run -it --rm -v /sys/fs/cgroup -v "$scriptpath"/nerdctl:/nerdctl --privileged --cap-add all nerdctl:test /scripts/s1_prepare.sh sh
-docker run -it --rm \
-    -v "$spath"/nerdctl:/nerdctl \
-    -v "$spath"/lib:/var/lib/containerd \
-    --privileged --cap-add all nerdctl:test /scripts/prepare.sh sh
+docker run -it --rm -v "$spath"/nerdctl:/nerdctl \
+                    -v "$spath"/lib:/var/lib/containerd \
+                    --privileged --cap-add all nerdctl:test /scripts/prepare.sh sh
 #   -v /sys/fs/cgroup:/sys/fs/cgroup \
